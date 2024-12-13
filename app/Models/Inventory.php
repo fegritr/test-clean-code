827beaa8b0aea90dc50671d1f8dedfb3d7fcebf8
Fauzi Achmad Haruna <fauziachmadharuna@Fauzis-MacBook-Pro.local>
2024-12-13 10:50:51 +0800

    Add migration

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Builders\InventoryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+    public function newEloquentBuilder($query)
+    {
+        return new InventoryBuilder($query);
+    }
 }
